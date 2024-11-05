52b7f4cfdbf59832aa0560adad3bb843a8bad13f
rahulreddyallu <rahulallu.career@gmail.com>
2024-11-05 11:40:43 +0530

    landing routing modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import Landing from './Components/Landing';
 import Work from './Components/Work';
 import About from './Components/About';
 import Contact from './Components/Contact';
@@ -12,7 +11,7 @@ function App() {
       <BrowserRouter>
         <NavBar/>
         <Routes>
-          <Route path='/' element={<Landing/>}/>
+          <Route path='/*' element={<Work/>}/>
           <Route path='/work' element={<Work/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/contact' element={<Contact/>}/>
